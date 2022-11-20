62460f87e97dd454ab7dc48238616307dcfd8136
sanjiv kannaa jeganathan <jsanjiv2003@gmail.com>
2022-11-20 08:49:14 +0530

    fix: main.c

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 
 ## Problem Statement
-- 6) Implementation of cut vertices and separability
+- PROBLEM 6 => Implementation of cut vertices and separability
 
 ## Algorithim
 - get the vertices
 - get the edges
+- loop through the vertices, remove the ith vertix and check if the resulted graph is connected
+    - if not connected => the ith vertix is a cut vertix and graph is called seperable
--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void check_seperablity(int vertices[], int number_of_vertices, int edges[], int number_of_edges, int removed){
+    
+}
+
+
 
 //using int main as return can be used to break the flow in case of invalid input
 int main(){
     //getting the vertices
-    int i, number_of_vertices, number_of_edges;
+    int number_of_vertices, number_of_edges;
     printf("Enter the number of vertices : ");
     scanf("%d", &number_of_vertices);
     int *vertices = malloc(number_of_vertices*sizeof(int));
-    for(i=0;i<number_of_vertices;i++){
+    for(int i=0;i<number_of_vertices;i++){
         printf("Enter the %dth vertix : ", i+1);
         scanf("%d", &vertices[i]);
     }
@@ -18,14 +23,14 @@ int main(){
     printf("\n\nEnter the number of edges : ");
     scanf("%d", &number_of_edges);
     int **edges = malloc(number_of_edges*sizeof(int*));
-    for(i=0;i<number_of_edges;i++){
+    for(int i=0;i<number_of_edges;i++){
         edges[i] = malloc(2*sizeof(int));
     }
-    for(i=0;i<number_of_edges;i++){
+    for(int i=0;i<number_of_edges;i++){
         printf("Enter the %dth vertix : ", i+1);
         scanf("%d %d", &edges[i][0], &edges[i][1]);
     }
-    for(i=0;i<number_of_edges;i++){
+    for(int i=0;i<number_of_edges;i++){
         int value[2] = {0, 0};
         for(int j=0;j<number_of_vertices;j++){
             if(edges[i][0] == vertices[j]){
@@ -45,9 +50,13 @@ int main(){
         printf("\n%dth vertix is : %d-%d", i+1, edges[i][0], edges[i][1]);
     }
     */
-   for(int v=0;v<number_of_vertices;v++){
-        for(int e=0;e<number_of_edges;e++){
-            // check_separability(v, e);
-        }
-   }
+
+    /*
+    loop i : vertix
+        loop j, k: vertix
+            check path from j to k
+    */
+    for(int removed=0;removed<number_of_vertices;removed++){
+        check_seperablity(vertices, number_of_vertices, edges, number_of_edges, removed);
+    }
 }
