6f9328608695046bbee109195b312779188a39c8
sanjiv kannaa jeganathan <84980036+SanjivKannaa@users.noreply.github.com>
2022-11-26 00:24:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,86 +1,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int adj_matrix[30][30], visited[30];
-int number_of_vertices;
+int *visited;
+int number_of_vertices, number_of_edges;
+int *vertices;
+int **edges;
+int seperable=0;
+int **adj_matrix;
 
-int check_connectivity(int v){
-    int i;
+
+void DFS(int v){
+    printf("%d ", v+1);
     visited[v] = 1;
-    for (i = 1; i <= number_of_vertices; i++){
-        if (adj_matrix[v][i] && !visited[i]){
-            printf("\n %d->%d", v, i);
-            check_connectivity(i);
+    for(int i=1;i<number_of_vertices;i++){
+        if(adj_matrix[v][i]==1 && visited[i]==0){
+            DFS(i);
         }
     }
-    int count = 0;
-    for (int i = 1; i <= number_of_vertices; i++) {
-        if (visited[i])
-            count++;
-    }
-    if (count == number_of_vertices)
-        return 1;
-    else
-        return 0;
 }
 
-//using int main as return can be used to break the flow in case of invalid input
 int main(){
     //getting the vertices
-    int number_of_edges;
     printf("Enter the number of vertices : ");
     scanf("%d", &number_of_vertices);
-    int *vertices = malloc(number_of_vertices*sizeof(int));
+    vertices = (int*)malloc(sizeof(int)*number_of_vertices);
     for(int i=0;i<number_of_vertices;i++){
-        printf("Enter the %dth vertix : ", i+1);
-        scanf("%d", &vertices[i]);
+        // printf("Enter the %dth vertix : ", i+1);
+        // scanf("%d", &vertices[i]);
+        vertices[i] = i+1;
     }
-
+    visited = (int*)malloc(sizeof(int)*number_of_vertices);
     //getting the edges
     printf("\n\nEnter the number of edges : ");
     scanf("%d", &number_of_edges);
-    int **edges = malloc(number_of_edges*sizeof(int*));
+    edges = malloc(sizeof(int*)*number_of_edges);
     for(int i=0;i<number_of_edges;i++){
-        edges[i] = malloc(2*sizeof(int));
-    }
-    for(int i=0;i<number_of_edges;i++){
-        printf("Enter the %dth vertix : ", i+1);
-        scanf("%d %d", &edges[i][0], &edges[i][1]);
-    }
-    for(int i=0;i<number_of_edges;i++){
-        int value[2] = {0, 0};
-        for(int j=0;j<number_of_vertices;j++){
-            if(edges[i][0] == vertices[j]){
-                value[0] = 1;
-            }
-            if(edges[i][1] == vertices[j]){
-                value[1] = 1;
-            }
-        }
-        if (value[0] == 0 || value[1] == 0){
-            // exiting the run if the edges have a vertix that does not exist
+        edges[i] = malloc(sizeof(int)*2);
+        printf("Enter the edge : ");
+        int v1, v2;
+        scanf("%d %d", &v1, &v2);
+        v1--;
+        v2--;
+        if(v1>number_of_vertices || v2>number_of_vertices){
+            printf("invalid vertice number detected");
             return 0;
         }
+        edges[i][0] = v1;
+        edges[i][1] = v2;
+    }
+    adj_matrix = (int**)malloc(sizeof(int*)*number_of_vertices);
+    for(int i=0;i<number_of_vertices;i++){
+        adj_matrix[i] = (int*)malloc(sizeof(int)*number_of_vertices);
     }
     for (int i = 0; i < number_of_edges; i++){
-        adj_matrix[edges[i][0]][edges[i][1]] = 1;
         adj_matrix[edges[i][1]][edges[i][0]] = 1;
+        adj_matrix[edges[i][0]][edges[i][1]] = 1;
     }
-    /*
-    //show all the edges
-    for(int i=0;i<number_of_edges;i++){
-        printf("\n%dth vertix is : %d-%d", i+1, edges[i][0], edges[i][1]);
-    }
-    */
-
-    /*
-    loop i : vertix
-        loop j, k: vertix
-            check path from j to k
-    */
     for (int i = 0; i < number_of_vertices; i++){
-        if (!check_connectivity(vertices[i])){
-            printf("%d ", vertices[i]);
+        for (int j = 0; j < number_of_vertices; j++){
+            printf("%d ", adj_matrix[i][j]);
         }
-    }    
+        printf("\n");
+    }
+    printf("\n\n");
+    DFS(0);
 }
